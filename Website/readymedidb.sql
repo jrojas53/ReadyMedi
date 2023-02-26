287c0bcbda998739db5196b6a6f16a5c9583bd43
MichaelEtchison <84116359+MichaelEtchison@users.noreply.github.com>
2023-02-25 16:42:07 -0800

    Update readymedidb.sql

--- Website/readymedidb.sql
+++ Website/readymedidb.sql
@@ -36,7 +36,7 @@ CREATE TABLE `Illness` (
 
 LOCK TABLES `Illness` WRITE;
 /*!40000 ALTER TABLE `Illness` DISABLE KEYS */;
-INSERT INTO `Illness` VALUES (1,'Valley Fever','fungal infection caused by coccidioides that are often breathed in'),(2,'Common Cold','viral infection of the nose and throat');
+INSERT INTO `Illness` VALUES (1,'Valley Fever','fungal infection caused by coccidioides that are often breathed in'),(2,'Common Cold','viral infection of the nose and throat'),(3,'Flu','viral infection of the respiratory tract caused by influenze viruses'),(4,'Strep Throat','infection of the back of the throat caused by the bacteria Streptococcus pyogenes'),(5,'Pink Eye','Conjunctivitis, inflammation of the outer layer of the white part of the eye'),(6,'Stomach Flu','Viral gastroenteritis, inflammation and irritation of stomach and intestines'),(7,'Chickenpox','viral illness caused by varicella zoster forming itchy blisters'),(8,'Pneumonia','inflammatory condition of the lung affecting alveoli by virus or bacteria'),(9,'Asthma','long-term inflammatory disease of the aiways of the lungs causing airflow obstruction'),(10,'COVID-19','highly contagious disease caused by severe acute respiratory syndrom coronavirus 2');
 /*!40000 ALTER TABLE `Illness` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -48,8 +48,8 @@ DROP TABLE IF EXISTS `Illness_Symptoms`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Illness_Symptoms` (
-  `name` text NOT NULL,
-  `description` text NOT NULL
+  `illid` int(11) DEFAULT NULL,
+  `name` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 `ENCRYPTION_KEY_ID`=100;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -59,7 +59,7 @@ CREATE TABLE `Illness_Symptoms` (
 
 LOCK TABLES `Illness_Symptoms` WRITE;
 /*!40000 ALTER TABLE `Illness_Symptoms` DISABLE KEYS */;
-INSERT INTO `Illness_Symptoms` VALUES ('Common Cold','Sore Throat'),('Valley Fever','Cough, Fatigue, Fever, Shortness of Breath, Headache, Night Sweats, Muscle aches or joint pain, Rash on upper body or legs');
+INSERT INTO `Illness_Symptoms` VALUES (1,'Sore Throat'),(1,'Cough'),(1,'Fever'),(1,'Shortness of Breath'),(2,'Cough'),(2,'Sneeze');
 /*!40000 ALTER TABLE `Illness_Symptoms` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,6 +86,29 @@ LOCK TABLES `Patient` WRITE;
 INSERT INTO `Patient` VALUES (1111,'Billy'),(1222,'Billy Bob'),(1333,'Billy Bob Joe');
 /*!40000 ALTER TABLE `Patient` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `Symptom`
+--
+
+DROP TABLE IF EXISTS `Symptom`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Symptom` (
+  `sympid` int(11) NOT NULL,
+  `name` text NOT NULL,
+  PRIMARY KEY (`sympid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 `ENCRYPTION_KEY_ID`=100;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Symptom`
+--
+
+LOCK TABLES `Symptom` WRITE;
+/*!40000 ALTER TABLE `Symptom` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Symptom` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -96,4 +119,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-21 18:56:53
+-- Dump completed on 2023-02-25 16:33:07
