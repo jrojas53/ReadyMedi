7836812b346382da0e773eb8e63b693053e71421
Kristine Coggshall <kcoggshall@artemis.cs.csub.edu>
2023-02-26 18:13:25 -0800

    php file to establish connection between website and MariaDB

---
+++ Website/.connect.php
@@ -0,0 +1,23 @@
+<?php
+//SQL connect
+//Kristine
+
+//connection
+
+$server = 'localhost';
+$user = 'readymedi';
+$password = 'Duc44Cpuzf';
+$database = 'readymedi';
+
+$db = new mysqli($server, $user, $password, $database);
+
+//check if connection fails
+if (mysqli_connect_error()) {
+	
+	die("Database connection failed: " . mysqli_connect_error());
+}
+/*else{
+	
+	echo "success";
+}*/
+?>
