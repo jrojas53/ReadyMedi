5fe8bd96af51ce69ebe438e562db6a2409538411
Jesus Rojas <jesusmrojas5991@gmail.com>
2023-04-13 17:04:43 -0700

    adding login.php register.php logout.php which have the basic css styles added, no php code yet - coming soon

---
+++ Website/logout.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>Account</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <ul>
+                <li><a class = "active" style="text-decoration: none"  href="logout.php"> Logout</a></li>
+            </ul>
+        </header>        
+        <?php
+            
+        ?>
+        <h1>Thank you for visiting our site!</h1>
+        
+        <div class ="footer">
+        <a class = "active" style="text-decoration: none" href="register.php">Register</a></li>
+            <a style="text-decoration: none" href="index.html"> Home</a></li>
+            <a style="text-decoration: none" href="about.html"> About</a></li>
+            <a style="text-decoration: none" href="contact.html"> Contact</a></li>
+        </div>
+    </body>
+</html>
