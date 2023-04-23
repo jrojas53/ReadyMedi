5b6c60e7a21a8c856484c4b435e3f6e1e29689f7
sflores64 <78442218+sflores64@users.noreply.github.com>
2023-04-23 16:45:50 -0700

    More questionnaire tinkering

--- Website/test_question_page.php
+++ Website/test_question_page.php
@@ -1,53 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>Demo</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <header>
-            <ul>
-                <li><a style="text-decoration: none" href="index.html"> Home</a></li>
-                <li><a class = "active" style="text-decoration: none" href="about.html"> About</a></li>
-                <li><a style="text-decoration: none" href="contact.html">Contact</a></li>
-            </ul>
-        </header>
-        <h1>Test Question Page</h1>
-        <div class="page_alt">
-                        <?php
-                        // include as it requires connection
-                        include '.connect.php';
-                        // Retrieve questions from database
-                        $sql = "SELECT * FROM quiz_questions";
-                        $result = $conn->query($sql);
+<?php
 
-                        // Display questions and options in a form
-                        if ($result->num_rows > 0) {
-                                echo "<form method='post'>";
-                                while($row = $result->fetch_assoc()) {
-                                        echo "<p>" . $row["question_text"] . "</p>";
-                                        echo "<input type='radio' name='question" . $row["question_id"] . "' value='answer1'>" . $row["answer1"] . "<br>";
-                                        echo "<input type='radio' name='question" . $row["question_id"] . "' value='answer2'>" . $row["answer2"] . "<br>";
-                                        echo "<input type='radio' name='question" . $row["question_id"] . "' value='answer3'>" . $row["answer3"] . "<br>";
-                                        echo "<input type='radio' name='question" . $row["question_id"] . "' value='answer4'>" . $row["answer4"] . "<br><br>";
-                                }
-                                echo "<input type='submit' name='submit' value='Submit'>";
-                                echo "</form>";
-                        }
-                            
-                        // Process form submission 
-                        // Display results
-                        //      echo "<p>Thank you for taking the quiz. Result functionality pending. .</p>";
+// check if connected to database
+// include as it requires connection.
+include '.connect.php';
+
+// This var holds the query statement we would like to run in MariaDB
+// The ';' is not required as php sanitizes itself.
+$sql_statement = "Select * from quiz_questions";
+
+// $result stores the output from passing the query statement to MDB.
+$result = $db->query($sql_statement);
+
+// $row stores the results PERMANENTALY to be used multiple times
+// rather than just query() that only stores temporarily.
+$row = $result->fetch_assoc();
+
+// num_rows is a keyword from php? that will store the number of rows received.
+$times = $result->num_rows;
+
+//echo "number of rows " . $result->num_rows;
+//echo "id: " . $row["illid"] . "- name: " . $row["name"] ."- description: " . $row[description];
+
+for ($i = 1; $i <= $times; $i++){
+	
+	// store new query statement depending on row $i
+	$test = "select * from test_questions where id = $i";
+	//echo "$test\n";
+	
+	// Grab results from the row we're requesting from.
+	$result = $db->query($test);
+	$row = $result->fetch_assoc();
+	
+	// pass collumn name into $row[] to print the wanted info.
+	echo "Question ID: " . $row["id"] . "\n" . "Question: " . $row["question_text"] ."\n" . "Possible Answers: " . $row["answer1"] . "\n\n";
+}
 ?>
-        </div>
-        <div class ="footer">
-            <a class = "active" style="text-decoration: none" href="demo.php">Demo</a>
-            <a style="text-decoration: none" href="index.html"> Home</a></li>
-            <a style="text-decoration: none" href="account.php"> Account</a></li>
-            <a style="text-decoration: none" href="about.html"> About</a></li>
-            <a style="text-decoration: none" href="contact.html"> Contact</a></li>
-        </div>
-    </body>
-</html>
+
